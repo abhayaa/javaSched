38dcc9aba5b684f5ddbb93b8ef60e9ab15243354
Abhaya Basnet <basnetabhaya@gmail.com>
2018-01-03 22:55:51 -0600

    Small changes

---
+++ Time.java
@@ -0,0 +1,17 @@
+package sched;
+/**
+ * 
+ * @author abhayabasnet
+ *
+ */
+
+/**
+ * A time class may be useful in solving the problem of not being able to
+ * effectively manipulate the time variables. Adding a field that marks am/pm
+ * might be a little more efficient/effective in the larger scheme of the
+ * project. Just a thought for now, but probably a good expansion for
+ * scalability.
+ */
+public class Time {
+
+}
